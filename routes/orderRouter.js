b07a0be3e2258450dfff4d2958df086dbb9523d4
JulluisW <julluisw@gmail.com>
2022-01-27 04:36:36 +0700

    get order and post order function

---
+++ routes/orderRouter.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const orderRouter = express.Router()
+const {Order, Shop} = require('../models/index.js')
+
+orderRouter.post('/add', async(req,res,next)=>{
+  try {
+    const {customerName, orderDestination, customerPhoneNumber,totalPrice} = req.body
+
+    const shop = await Shop.findOne({
+      where: {
+        userId : req.currentUser.userId
+      }
+    })
+
+    const resp = await Order.create({
+      customerName, 
+      orderDestination, 
+      customerPhoneNumber,
+      totalPrice,
+      status: "pending",
+      shopId : shop.id
+    })
+
+    res.status(201).json(resp)
+  } catch (error) {
+    next(error)
+  }
+})
+
+orderRouter.get('/', async(req,res,next)=>{
+  try {
+    const shop = await Shop.findOne({
+      where: {
+        userId : req.currentUser.userId
+      }
+    })
+
+    const resp = await Order.findAll({
+      where: {
+        shopId: shop.id
+      }
+    })
+
+    res.status(200).json(resp)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports= orderRouter
